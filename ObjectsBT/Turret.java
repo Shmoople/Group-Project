9a9ecedff662a76fe41565300e12be92050cedd8
ShortsATWIT <shorts@wit.edu>
2022-12-02 16:05:57 -0500

    Baloon and Turret classes

---
+++ ObjectsBT/Turret.java
@@ -0,0 +1,18 @@
+package ObjectsBT;
+
+public class Turret {
+		double health = 1.0;
+		double dmg = 1.0;
+		double fire = 1.5;
+
+		public class Turret2 {
+			double health = 1.0;
+			double dmg = 1.0;
+			double fire = 1.25;
+		}
+		public class Turret3 {
+			double health = 1.0;
+			double dmg = 1.5;
+			double fire = 1.0;
+		}
+}
